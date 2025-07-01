3093a43e3a621e2f377b320f35417296813e426e
Neelam Dheeraj Kumar <neelamdheerajkumar1122@gmail.com>
2025-07-01 16:31:50 +0530

    Dockercfiles
    create an docker-compose file

---
+++ docker-main/flask-app/flask-app/app.py
@@ -0,0 +1,11 @@
+hi hello
+
+this is Dheeraj 
+
+From Flask-app
+
+
+<h1>
+ Welcome to Flask app 
+ </h1>
+
---
+++ docker-main/node-app/node-app/app.js
@@ -0,0 +1,11 @@
+Hello everyone
+
+this is Dheeraj Form
+
+<h1>  
+    Welcome to node.js app!
+    </h1>
+
+    <h2>
+        this is Dheeraj
+        </h2>
